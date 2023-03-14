b09d1f49a5a8e229fc0d959f933d0033ce04bef9
Dipankar <dipankar15-11137@diu.edu.bd>
2023-03-14 14:02:28 +0600

    appointment banner add

--- src/Components/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Components/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import { format } from "date-fns";
 
 const AppointmentBanner = () => {
+  const [selectDate, setSelectDate] = useState(new Date());
   return (
     <header>
-      <h1>Banner</h1>
+      <div className="hero mt-16">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://media.istockphoto.com/id/1080232656/photo/female-teacher-pointing-with-finger-at-mathematical-equation-on-chalkboard-in-class.jpg?s=612x612&w=0&k=20&c=F3T62Mo1lj0n0vP44gAPDuv52h2ZrB4ggNAFdGnA2M0="
+            alt="teacher"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div className="mr-20">
+            <DayPicker
+              mode="single"
+              selected={selectDate}
+              onSelect={setSelectDate}
+            />
+          </div>
+          <p>You have select date : {format(selectDate, "PP")}</p>
+        </div>
+      </div>
     </header>
   );
 };
---
+++ src/Components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AvailableAppointment = () => {
+  return (
+    <div>
+      <p>Available Appointment</p>
+    </div>
+  );
+};
+
+export default AvailableAppointment;
