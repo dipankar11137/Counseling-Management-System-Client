d8fdd0504ac5a7bb37cd7d68f88fa949acf33663
Dipankar <dipankar15-11137@diu.edu.bd>
2023-03-19 13:11:19 +0600

    manage complain

--- src/Components/Pages/Dashboard/ManageCounseling/ManageCounseling.js
+++ src/Components/Pages/Dashboard/ManageCounseling/ManageCounseling.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-const ManageCounseling = () => {
+const ManageCounseling = ({ counseling }) => {
   return (
-    <div>
-      <h1>hmm</h1>
-    </div>
+    <tr>
+      <th>
+        <div className="avatar">
+          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src={counseling?.studentImg} alt="img" />
+          </div>
+        </div>
+      </th>
+      <td>{counseling?.studentName}</td>
+      <td>{counseling?.studentID}</td>
+      <td>{counseling?.appointmentDate}</td>
+      <td>{counseling?.slot}</td>
+      <td>{counseling?.phone}</td>
+      <td>{counseling?.problem}</td>
+      <td>
+        <button className="btn btn-sm font-bold">Accept</button>
+      </td>
+    </tr>
   );
 };
 
--- src/Components/Pages/Dashboard/ManageCounseling/ManageCounselings.js
+++ src/Components/Pages/Dashboard/ManageCounseling/ManageCounselings.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ManageCounseling from "./ManageCounseling";
 
 const ManageCounselings = () => {
   const [counselings, setCounselings] = useState([]);
@@ -7,9 +8,32 @@ const ManageCounselings = () => {
       .then((res) => res.json())
       .then((data) => setCounselings(data));
   }, []);
+  console.log(counselings);
   return (
     <div>
-      <h1>Hello{counselings.length}</h1>
+      <table className="table w-full">
+        {/* head*/}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Slot</th>
+            <th>Phone</th>
+            <th>Problem</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {counselings.map((counseling) => (
+            <ManageCounseling
+              key={counseling._id}
+              counseling={counseling}
+            ></ManageCounseling>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
