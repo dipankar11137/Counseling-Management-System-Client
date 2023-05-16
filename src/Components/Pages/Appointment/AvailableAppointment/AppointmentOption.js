ae50a3beb162a9e97bfb6197de0142e9e2a9b834
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-16 13:58:50 +0600

    off day

--- src/Components/Pages/Appointment/AvailableAppointment/AppointmentOption.js
+++ src/Components/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AppointmentOption = ({ option, setCounseling }) => {
+const AppointmentOption = ({ option, setCounseling, day }) => {
   const { name, slots } = option;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
@@ -25,19 +25,45 @@ const AppointmentOption = ({ option, setCounseling }) => {
         <h2 className=" text-xl">Designation : Assistant Professor </h2>
         <h2 className="text-xl">Department : Department of CSE </h2>
 
-        <p className="text-xl font-bold text-blue-600 text-center">
-          {slots.length > 0 ? (
-            slots[0]
-          ) : (
-            <span className="text-red-500">Try Another Day</span>
-          )}
-        </p>
-        <p className="text-lg text-green-900 font-semibold text-center">
-          {slots.length} {slots.length > 1 ? "spaces" : "space"}
-        </p>
+        {day === option?.day ? (
+          <>
+            <h1 className="text-center text-xl font-bold">
+              This Day is Off day
+            </h1>
+            <h1 className="text-red-500 text-xl font-bold text-center">
+              Try Another Day
+            </h1>
+          </>
+        ) : (
+          <>
+            {" "}
+            <p className="text-xl font-bold text-blue-600 text-center">
+              {slots.length > 0 ? (
+                slots[0]
+              ) : (
+                <span className="text-red-500">Try Another Day</span>
+              )}
+            </p>
+            <p className="text-lg text-green-900 font-semibold text-center">
+              {slots.length} {slots.length > 1 ? "spaces" : "space"}
+            </p>
+          </>
+        )}
+        {/* <div>
+          <p className="text-xl font-bold text-blue-600 text-center">
+            {slots.length > 0 ? (
+              slots[0]
+            ) : (
+              <span className="text-red-500">Try Another Day</span>
+            )}
+          </p>
+          <p className="text-lg text-green-900 font-semibold text-center">
+            {slots.length} {slots.length > 1 ? "spaces" : "space"}
+          </p>
+        </div> */}
         <div className="card-actions justify-center">
           <label
-            disabled={slots.length === 0}
+            disabled={slots.length === 0 || day === option?.day}
             onClick={() => setCounseling(option)}
             htmlFor="booking-modal"
             className="btn bg-indigo-700 text-white font-bold"
--- src/Components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -7,7 +7,10 @@ import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointment = ({ selectDate }) => {
   const [counseling, setCounseling] = useState(null);
-  const date = format(selectDate, "PP");
+  const date = format(selectDate, "PP", "MMMM d");
+  console.log(date);
+  const day = format(selectDate, "EEEE");
+  console.log(day);
 
   const {
     data: appointmentOptions = [],
@@ -25,8 +28,8 @@ const AvailableAppointment = ({ selectDate }) => {
   }
   return (
     <section className="mt-6 mx-40 mb-20">
-      <p className="text-3xl text-center mt-10 text-green-900 font-semibold mb-16">
-        Available Appointment on {format(selectDate, "PP")}
+      <p className="text-3xl  text-center mt-10 text-green-900 font-bold mb-16">
+        Available Appointment on {day}, {format(selectDate, "PP")}
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-36 mt-40">
         {appointmentOptions.map((option) => (
@@ -34,6 +37,7 @@ const AvailableAppointment = ({ selectDate }) => {
             key={option._id}
             option={option}
             setCounseling={setCounseling}
+            day={day}
           ></AppointmentOption>
         ))}
       </div>
